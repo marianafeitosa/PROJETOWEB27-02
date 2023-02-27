971646de6a50319b338817fd0402359a74dc339a
marips <99839074+marianafeitosa@users.noreply.github.com>
2023-02-27 15:18:45 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+// Criação de variável 
+
+$a = 0;
+
+// Sistema de decisão 
+$b= 10;
+if($b <= 10){
+    echo "O valor da variável b é maior ou igual a 10!"
+
+
+}else{
+    echo "O valor da variável b é menor que 10! "
+}
+
+//Laços de repetição 
+for($i = 0; $i <= 10;){
+    echo $i;
+}
+
+$x = 0;
+while($x <= 10){
+    echo $x;
+}
+
+$y = 0;
+do{
+    echo $y;
+} while($y <= 10);
+
+
+?>
+    
+
